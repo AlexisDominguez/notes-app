e153cc5863463d13b7462bfadf15f8c3c952e4e4
Alexis Ariel Domínguez Sánchez <38707921+AlexisDominguez@users.noreply.github.com>
2022-07-09 17:53:25 -0600

    Feat: todo list table with mock data (#4)
    Co-authored-by: Alexis Dominguez <alexis.dominguez@truehome.com.mx>

---
+++ src/components/TodoListTable/TodoListTable.tsx
@@ -0,0 +1,12 @@
+import DataTable from "react-data-table-component";
+import data from "../../mocks/components/TodoListTable/data.json";
+import { Todo } from "../../models/Todo";
+import { TODO_LIST_TABLE_COLUMNS } from "./constants";
+
+const TodoListTable = () => (
+  <div>
+    <DataTable columns={TODO_LIST_TABLE_COLUMNS} data={data as Todo[]} />
+  </div>
+);
+
+export default TodoListTable;
